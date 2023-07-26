7aa7e43ed514fe64cc8fee868cbbe06bff82fec3
Memeladon <bingorussian@mail.ru>
2023-07-26 08:03:41 +0300

    Complete?

--- Scripts/clustering.py
+++ Scripts/clustering.py
@@ -17,27 +17,29 @@ class Clustering:
                 except ValueError:
                     continue
 
-        kmeans = KMeans(n_clusters=5)
-        kmeans.fit(numeric_data)
-        labels = kmeans.labels_
-
-        plt.scatter(numeric_data[:, 0], numeric_data[:, 1], c=labels)
-        plt.xlabel('Feature 1')
-        plt.ylabel('Feature 2')
-        plt.show()
-
-        """
-        numeric_data = np.array([[r['Aborted_clients'], r['Latency']] for r in self.data])
         kmeans = KMeans(n_clusters=3)
         kmeans.fit(numeric_data)
         labels = kmeans.labels_
 
-        for i in range(3):
-            cluster_data = [numeric_data[j] for j in range(len(numeric_data)) if labels[j] == i]
-            cluster_data = list(zip(*cluster_data))
-            plt.scatter(cluster_data[0], cluster_data[1])
-            plt.xlabel('X')
-            plt.ylabel('Y')
-            plt.title('Cluster ' + str(i))
-            plt.show()
-        """
+        self.plot_clusters(numeric_data, labels)
+
+        # """
+        # for i in range(3):
+        #     cluster_data = [numeric_data[j] for j in range(len(numeric_data)) if labels[j] == i]
+        #     cluster_data = list(zip(*cluster_data))
+        #     plt.scatter(cluster_data[0], cluster_data[1])
+        #     plt.xlabel('Data Point Index')
+        #     plt.ylabel('Data')
+        #     plt.title('Cluster ' + str(i))
+        #     plt.colorbar(label='Latency (ms)')
+        #     plt.show()
+        # """
+
+    @staticmethod
+    def plot_clusters(numeric_data, labels):
+        plt.scatter(numeric_data[:, 0], numeric_data[:, 1], c=labels, cmap='viridis')
+        plt.xlabel('Latency')
+        plt.ylabel('Data Point Index')
+        plt.colorbar(label='Latency (ms)')
+        plt.show()
+
