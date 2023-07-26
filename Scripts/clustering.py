340175760c56249e4eab0bd5aa7366680b6b3d9a
Memeladon <bingorussian@mail.ru>
2023-07-26 08:47:54 +0300

    Complete

--- Scripts/clustering.py
+++ Scripts/clustering.py
@@ -16,30 +16,30 @@ class Clustering:
                     numeric_data[i][j] = float(item[key])
                 except ValueError:
                     continue
+        print(numeric_data)
 
         kmeans = KMeans(n_clusters=3)
         kmeans.fit(numeric_data)
         labels = kmeans.labels_
 
-        self.plot_clusters(numeric_data, labels)
+        # self.plot_clusters(numeric_data, labels)
 
         # """
-        # for i in range(3):
-        #     cluster_data = [numeric_data[j] for j in range(len(numeric_data)) if labels[j] == i]
-        #     cluster_data = list(zip(*cluster_data))
-        #     plt.scatter(cluster_data[0], cluster_data[1])
-        #     plt.xlabel('Data Point Index')
-        #     plt.ylabel('Data')
-        #     plt.title('Cluster ' + str(i))
-        #     plt.colorbar(label='Latency (ms)')
-        #     plt.show()
+        for i in range(3):
+            cluster_data = [numeric_data[j] for j in range(len(numeric_data)) if labels[j] == i]
+            cluster_data = list(zip(*cluster_data))
+            plt.scatter(cluster_data[0], cluster_data[1])
+            plt.xlabel('Data Point Index')
+            plt.ylabel('total weight of data')
+            plt.title('Cluster ' + str(i))
+            plt.show()
         # """
 
-    @staticmethod
-    def plot_clusters(numeric_data, labels):
-        plt.scatter(numeric_data[:, 0], numeric_data[:, 1], c=labels, cmap='viridis')
-        plt.xlabel('Latency')
-        plt.ylabel('Data Point Index')
-        plt.colorbar(label='Latency (ms)')
-        plt.show()
+    # @staticmethod
+    # def plot_clusters(numeric_data, labels):
+    #     plt.scatter(numeric_data[:, 0], numeric_data[:, 1], c=labels, cmap='viridis')
+    #     plt.xlabel('Data Point Index')
+    #     plt.ylabel('Latency')
+    #     plt.colorbar(label='data complexity')
+    #     plt.show()
 
